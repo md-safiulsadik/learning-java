48fc57b051cc37db103a055cca2d2ea64002e898
md-safiulsadik <md-safiulsadik@users.noreply.github.com>
2025-07-20 12:53:56 +0600

    added new answers

--- NetBeansProjects/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ NetBeansProjects/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,11 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter counter = new Counter(100);
+        
+        counter.increase(100);
+        counter.decrease(-50);
+        
+        System.out.println(counter);
     }
 }
