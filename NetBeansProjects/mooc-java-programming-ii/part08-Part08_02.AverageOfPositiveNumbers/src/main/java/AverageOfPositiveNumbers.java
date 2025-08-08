1f31375c2a37736e3d1f361addb5685325955cd5
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-08-08 20:11:59 +0600

    solved p08-02

--- NetBeansProjects/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ NetBeansProjects/mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,36 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> nums = new ArrayList<>();
+        
+        while (true) {
+            int num = scanner.nextInt();
+            
+            if (num == 0) {
+                break;
+            }
+            
+            if (num > 0) {
+                nums.add(num);
+            }
+            
+        }
+        
+        if (nums.isEmpty()) {
+//            System.out.println(nums.size());
+            System.out.println("Cannot calculate the average");
+        } else {
+            int sum = 0;
+            for (Integer num : nums) {
+                sum += num;
+            }
 
+            System.out.println(sum / (double) nums.size());
+        }
     }
 }
