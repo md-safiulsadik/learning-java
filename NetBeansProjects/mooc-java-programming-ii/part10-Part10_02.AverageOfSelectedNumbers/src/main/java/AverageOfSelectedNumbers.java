0bdefa1f8220e29ccb12b5a35c6653e564916a76
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-14 20:31:31 +0600

    solved p10-02

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,64 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
-
+        List<String> inputs = new ArrayList<>();
+        // Write your program here
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                break;
+            }
+            
+            inputs.add(input);
+        }
+        
+        System.out.println("Print the average of the negative"
+                + " numbers or the positive numbers? (n/p)");
+        
+        String choice = scanner.nextLine();
+        
+        switch(choice) {
+            case "n":
+                printNegative(inputs);
+                break;
+            case "p":
+                printPositive(inputs);
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+        
+        
+    }
+    
+    public static void printPositive(List<String> inputs) {
+        
+        double avg = inputs.stream()
+                .mapToInt(Integer::parseInt)
+                .filter(s -> s > 0)
+                .average()
+                .getAsDouble();
+                
+        System.out.println("Average of the positive numbers: " + avg);
+    }
+    
+    public static void printNegative(List<String> inputs) {
+        
+        double avg = inputs.stream()
+                .mapToInt(Integer::parseInt)
+                .filter(s -> s < 0)
+                .average()
+                .getAsDouble();
+        
+        System.out.println("Average of the negative numbers: " + avg);
     }
 }
