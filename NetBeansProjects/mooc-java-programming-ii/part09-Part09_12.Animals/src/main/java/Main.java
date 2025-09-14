c5aa560aee7744de36a81dda80d3b0853800ca0c
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-14 20:30:40 +0600

    solved p09-12

--- NetBeansProjects/mooc-java-programming-ii/part09-Part09_12.Animals/src/main/java/Main.java
+++ NetBeansProjects/mooc-java-programming-ii/part09-Part09_12.Animals/src/main/java/Main.java
@@ -3,6 +3,21 @@ public class Main {
 
     public static void main(String[] args) {
         // you can test how your classes work here
+        
+        Dog dog = new Dog();
+        dog.bark();
+        dog.eat();
+
+        Dog fido = new Dog("Fido");
+        fido.bark();
+        
+//        NoiseCapable dog = new Dog();
+        dog.makeNoise();
+
+        NoiseCapable cat = new Cat("Garfield");
+        cat.makeNoise();
+        
+        ((Cat) cat).purr();
 
     }
 
