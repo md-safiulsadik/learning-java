2086a36faf55713200e31d69c690e2212c746699
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-14 20:31:15 +0600

    solved p10-01

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,12 +1,30 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
         // Write your program here
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                break;
+            }
+            
+            inputs.add(input);
+        }
+        
+        double avg = inputs.stream()
+                .mapToInt(s -> Integer.parseInt(s))
+                .average()
+                .getAsDouble();
 
+        System.out.println(avg);
     }
 }
