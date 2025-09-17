4e71718cde0174348ff0885677269a54916e90fc
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-17 14:49:48 +0600

    solved p10-10

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -2,8 +2,10 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
@@ -11,7 +13,15 @@ public class BooksFromFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        Set<String> names = new HashSet<>();
+        names.add("first");
+        names.add("first");
+        names.add("second");
+        names.add("second");
+        names.add("second");
+        
+        names.stream().forEach(System.out::println);
+        
     }
 
 }
