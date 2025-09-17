a5a595229dfa05adc1c3ef83aaf0166c342ddbb4
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-17 14:49:09 +0600

    solved p10-07

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_07.UniqueLastNames/src/main/java/UniqueLastNames.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_07.UniqueLastNames/src/main/java/UniqueLastNames.java
@@ -32,5 +32,11 @@ public class UniqueLastNames {
         }
 
         // Implement the printing of the unique last names in alphabetical order here:
+
+        persons.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
