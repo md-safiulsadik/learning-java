a0432d4b4d8a5c035d00ed5381aa5e7fbc687a0b
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-17 14:48:39 +0600

    solved p10-05

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,16 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> inputs = new ArrayList<>();
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) break;
+            
+            inputs.add(input);
+        }
+        
+        inputs.stream().forEach(System.out::println);
     }
 }
