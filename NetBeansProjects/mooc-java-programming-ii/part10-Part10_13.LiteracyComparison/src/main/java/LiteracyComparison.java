4fdc5fc0fb71dbe3197c10c6cbe74fdfea2b41bf
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-22 09:51:43 +0600

    solved p10-13

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LiteracyComparison {
     
     public static void main(String[] args) {
+ 
 
+        try {
+            List<Literacy> rates = Files.lines(Paths.get("literacy.csv"))
+                    .map(parts -> parts.split(","))
+                    .map(parts -> new Literacy(
+                            parts[2].replace("(%)", "").trim(),
+                            parts[3].trim(),
+                            Integer.parseInt(parts[4]),
+                            Double.parseDouble(parts[5])
+                    ))
+                    .collect(Collectors.toList());
+                    
+            rates.stream()
+                    .sorted((s1, s2) -> Double.compare(s1.getRate(), s2.getRate()))
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
     }
 }
