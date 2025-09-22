8f179bdbf8a2d073056b8852ffe19a7ca24ef36a
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-09-22 09:52:25 +0600

    test-file

---
+++ Projects/MyFirstProject/src/ReadFiles.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFiles {
+
+    List<String> literacyRate = new ArrayList<>();
+
+//    try {
+//        literacyRate = Files.lines(Paths.get("literacy.csv"))
+//                .map(parts -> parts.trim().split(","))
+//                .toList();
+//    } catch (IOException e) {
+//        System.out.println("Error: " + e.getMessage());
+//    }
+}
