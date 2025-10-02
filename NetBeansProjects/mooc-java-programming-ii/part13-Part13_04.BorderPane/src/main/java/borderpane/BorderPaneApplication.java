62f8c5de9592c0060d80ff7dc04f044fa76ce3f2
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-02 08:35:46 +0600

    solved p13-04

--- NetBeansProjects/mooc-java-programming-ii/part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ NetBeansProjects/mooc-java-programming-ii/part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,34 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneApplication {
+
+public class BorderPaneApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        
+        layout.setTop(new Label("NORTH"));
+        layout.setRight(new Label("EAST"));
+        layout.setBottom(new Label("SOUTH"));
+        
+        
+        Scene scene = new Scene(layout);
+        
+        window.setTitle("Border Pane");
+        window.setScene(scene);
+        window.show();
     }
 
 }
