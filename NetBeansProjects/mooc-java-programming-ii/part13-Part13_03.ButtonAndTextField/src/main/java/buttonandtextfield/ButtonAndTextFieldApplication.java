ffb69eeb765d13a80d0dbe61fa149e6672ea5c87
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-02 08:35:27 +0600

    solved p13-03

--- NetBeansProjects/mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ NetBeansProjects/mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,35 @@
 package buttonandtextfield;
 
 
-public class ButtonAndTextFieldApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class ButtonAndTextFieldApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class, args);
+    }
+
+    @Override
+    public void start(Stage window) {
+        FlowPane components = new FlowPane();
+        TextField labelText = new TextField("Write a massage!");
+        Button button = new Button("Click");
+        
+        components.getChildren().add(labelText);
+        components.getChildren().add(button);
+        
+        Scene scene = new Scene(components);
+        
+        window.setScene(scene);
+        window.setTitle("TextFild");
+        window.show();
     }
 
 }
