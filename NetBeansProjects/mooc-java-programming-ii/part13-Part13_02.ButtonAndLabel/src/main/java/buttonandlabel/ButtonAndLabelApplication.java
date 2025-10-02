391cd9864c17b7575870c99d97b05c84eb9cab23
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-02 08:35:05 +0600

    solved p13-02

--- NetBeansProjects/mooc-java-programming-ii/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ NetBeansProjects/mooc-java-programming-ii/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,35 @@
 package buttonandlabel;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;  // ✅ JavaFX Button
+import javafx.scene.control.Label;  // ✅ JavaFX Label
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndLabelApplication {
+
+public class ButtonAndLabelApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       launch(args);
+    }
+
+    @Override
+    public void start(Stage window) {
+        Label testLabel = new Label("This is a label");
+        Button button = new Button("Click");
+        
+        FlowPane compoGroup = new FlowPane();
+        
+        compoGroup.getChildren().add(testLabel);
+        compoGroup.getChildren().add(button);
+        
+        Scene scene = new Scene(compoGroup);
+        
+        window.setScene(scene);
+        window.setTitle("Button and Labels");
+        window.show();
     }
 
 }
