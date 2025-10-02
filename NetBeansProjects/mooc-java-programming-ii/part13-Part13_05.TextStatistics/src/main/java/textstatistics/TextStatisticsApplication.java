8342248c8f1ffd25fc03e3a3ae4301b0280d173c
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-02 08:36:00 +0600

    solved p13-05

--- NetBeansProjects/mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ NetBeansProjects/mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,45 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+
+public class TextStatisticsApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        launch(args);
+       
+    }
+
+    @Override
+    public void start(Stage window) {
+        
+        BorderPane layout = new BorderPane();
+        HBox hBox = new HBox();
+        
+        TextArea input = new TextArea();
+        input.setPromptText("Write us...");
+        
+        hBox.getChildren().add(new Label("Letters: 0"));
+        hBox.getChildren().add(new Label("Words: 0"));
+        hBox.getChildren().add(new Label("The longest word is:"));
+        
+        
+        layout.setCenter(input);
+        layout.setBottom(hBox);
+        
+        Scene scene = new Scene(layout);
+        
+        window.setScene(scene);
+        window.setTitle("Text statistics");
+        window.show();
     }
 
 }
