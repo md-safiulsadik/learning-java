b049a284f110fc08ba16261f77694c01cb44bcf6
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-02 08:34:48 +0600

    solved p13-01

--- NetBeansProjects/mooc-java-programming-ii/part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
+++ NetBeansProjects/mooc-java-programming-ii/part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
@@ -1,11 +1,19 @@
 package myFirstApplication;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MyFirstApplication {
-    
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class MyFirstApplication extends Application{
+
+    @Override
+    public void start(Stage window) {
+        window.setTitle("My first application");
+        window.show();
     }
 
+    public static void main(String[] args) {
+//        launch(MyFirstApplication.class);
+        launch(args);
+    }
 }
