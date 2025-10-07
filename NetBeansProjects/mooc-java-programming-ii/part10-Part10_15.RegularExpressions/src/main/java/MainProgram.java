d1912f07aa0d95e96a05f875ef87b4b4a172f238
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-07 11:36:51 +0600

    solved p10-15

--- NetBeansProjects/mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,13 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        
+        Checker checker = new Checker();
+        
+        if (checker.isDayOfWeek("tue")) {
+            System.out.println("The form is correct");
+        } else {
+            System.out.println("The form is incorrect");
+        }
     }
 }
